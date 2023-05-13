5de9bac56d67f9b10c440f010506923df046ae91
JohnnyFangFangFang <johnny781222@gmail.com>
2023-05-13 19:01:39 +0800

    Finish Cart calculation function

--- src/components/Main/Cart/Cart.jsx
+++ src/components/Main/Cart/Cart.jsx
@@ -1,21 +1,87 @@
+import { useState } from 'react';
+
 import styles from "./Cart.module.scss";
 import CartItems from "./CartItems.jsx";
 
-// 之後階段再做運費、小計的計算邏輯
+// 測試用資料
+const cartItemsData = [
+  {
+    id: '1',
+    name: '貓咪罐罐',
+    img: 'https://picsum.photos/300/300?text=1',
+    price: 100,
+    quantity: 2,
+  },
+  {
+    id: '2',
+    name: '貓咪干干',
+    img: 'https://picsum.photos/300/300?text=2',
+    price: 200,
+    quantity: 1,
+  },
+]
+
+// 計算用資料
+const cartItemsDataForCal = cartItemsData.map(cartItemData => {
+  return { ...cartItemData }
+})
+
+
+// 目前先做兩個 item 的計算邏輯，之後要想如何優化
+// 本單元題目尚未提及運費邏輯，故先以 0 計算
 export default function Cart() {
+
+  const [quantity1, setQuantity1] = useState(cartItemsDataForCal[0].quantity)
+  const [quantity2, setQuantity2] = useState(cartItemsDataForCal[1].quantity)
+
+  const totalPrice = cartItemsDataForCal[0].quantity * cartItemsDataForCal[0].price + cartItemsDataForCal[1].quantity * cartItemsDataForCal[1].price
+  const deliveryFee = 0
+
+  function handleIncreaseItem1Click() {
+    // 不太懂為何裡面直接寫 quantity1 + 1 會沒反應
+    setQuantity1(
+      cartItemsDataForCal[0].quantity = cartItemsDataForCal[0].quantity + 1
+    )
+  }
+
+  function handleIncreaseItem2Click() {
+    setQuantity2(
+      cartItemsDataForCal[1].quantity = cartItemsDataForCal[1].quantity + 1
+    )
+  }
+
+  function handleDecreaseItem1Click() {
+    // 商品數量不能有負數
+    if (cartItemsDataForCal[0].quantity > 0) {
+      setQuantity1(
+        cartItemsDataForCal[0].quantity = cartItemsDataForCal[0].quantity - 1
+      )
+    }
+  }
+
+  function handleDecreaseItem2Click() {
+    if (cartItemsDataForCal[1].quantity > 0) {
+      setQuantity2(
+        cartItemsDataForCal[1].quantity = cartItemsDataForCal[1].quantity - 1
+      )
+    }
+  }
+
+  console.log(cartItemsDataForCal)
+
   return (
     <section className={`${styles.cartContainer} col col-lg-5 col-sm-12`}>
       <h3 className={styles.cartTitle}>購物籃</h3>
       <section className={`col col-12`} data-total-price={0}>
-        <CartItems />
+        <CartItems data={cartItemsDataForCal} increase1={handleIncreaseItem1Click} increase2={handleIncreaseItem2Click} decrease1={handleDecreaseItem1Click} decrease2={handleDecreaseItem2Click} />
       </section>
       <section className={`${styles.cartInfo} shipping col col-12`}>
         <div className={styles.text}>運費</div>
-        <div className={styles.price} >免費</div>
+        <div className={styles.price} >{deliveryFee}</div>
       </section>
       <section className={`${styles.cartInfo} total col col-12`}>
         <div className={styles.text}>小計</div>
-        <div className={styles.price} >$300</div>
+        <div className={styles.price} >{totalPrice + deliveryFee}</div>
       </section>
     </section>
   )
--- src/components/Main/Cart/CartItems.jsx
+++ src/components/Main/Cart/CartItems.jsx
@@ -4,65 +4,50 @@ import styles from "./CartItems.module.scss";
 import minus from "assets/icons/minus.svg";
 import plus from "assets/icons/plus.svg";
 
-// 測試用資料
-const cartItemsData = [
-  {
-    id: '1',
-    name: '貓咪罐罐',
-    img: 'https://picsum.photos/300/300?text=1',
-    price: 100,
-    quantity: 2,
-  },
-  {
-    id: '2',
-    name: '貓咪干干',
-    img: 'https://picsum.photos/300/300?text=2',
-    price: 200,
-    quantity: 1,
-  },
-]
-
-function CartItem({ _id, name, img, price, quantity }) {
-  return (
-    <div
-      className={`${styles.productContainer} col col-12`}
-      data-count={quantity}
-      data-price={price}
-    >
-      <img
-        className={styles.imgContainer}
-        src={img}
-        alt="cart item img"
-      />
-      <div className={styles.productInfo}>
-        <div className={styles.productName}>{name}</div>
-        <div className={styles.productControlContainer}>
-          <div className={styles.productControl}>
-            <img
-              className={styles.productAction}
-              src={minus}
-              alt="minus.svg"
-            />
-            <span className={styles.productCount} >{quantity}</span>
-            <img
-              className={styles.productAction}
-              src={plus}
-              alt="plus.svg"
-            />
+// 產生購物車內已選購項目，若數量按到 0 會從購物車清單移除
+function CartItem({ id, name, img, price, quantity, increase1, increase2, decrease1, decrease2 }) {
+  if (quantity > 0) {
+    return (
+      <div
+        className={`${styles.productContainer} col col-12`}
+        data-count={quantity}
+        data-price={price}
+      >
+        <img
+          className={styles.imgContainer}
+          src={img}
+          alt="cart item img"
+        />
+        <div className={styles.productInfo}>
+          <div className={styles.productName}>{name}</div>
+          <div className={styles.productControlContainer}>
+            <div className={styles.productControl}>
+              <img onClick={id === '1' ? decrease1 : decrease2}
+                className={styles.productAction}
+                src={minus}
+                alt="minus.svg"
+              />
+              <span className={styles.productCount} >{quantity}</span>
+              <img onClick={id === '1' ? increase1 : increase2}
+                className={styles.productAction}
+                src={plus}
+                alt="plus.svg"
+              />
+            </div>
           </div>
+          <div className={styles.price} >{price * quantity}</div>
         </div>
-        <div className={styles.price} >${price}</div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 // 將購物車內所有已加入的商品清單匯出
-export default function CartItems() {
+export default function CartItems({ data, increase1, increase2, decrease1, decrease2 }) {
   return (
     <>
-      {cartItemsData.map(cartItem =>
-        <CartItem {...cartItem} key={cartItem.id} />
+      {data.map(cartItem =>
+        <CartItem {...cartItem} key={cartItem.id} increase1={increase1} increase2={increase2} decrease1={decrease1} decrease2={decrease2} />
       )}
     </>
   )
